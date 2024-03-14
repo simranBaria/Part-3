9aff2ea2a6cb934383da6a2149641d03e18fc76a
simranBaria <simranbaria06@gmail.com>
2024-03-14 18:07:53 -0400

    Task 4b complete: UI displays the currently selected character

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
 
 public class CharacterControl : MonoBehaviour
 {
     public static Villager SelectedVillager { get; private set; }
+    public TextMeshProUGUI selectedText;
+
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
@@ -15,5 +19,10 @@ public class CharacterControl : MonoBehaviour
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
     }
-    
+
+    private void Update()
+    {
+        if (SelectedVillager != null) selectedText.SetText("Selected: " + SelectedVillager.name);
+        else selectedText.SetText("Selected: None");
+    }
 }
