9fa9397e606ce421707a566810bfd2dba3744f64
simranBaria <simranbaria06@gmail.com>
2024-03-14 14:25:07 -0400

    Task 1 complete: archer and merchant made

---
+++ Assets/Week 9/Scripts/Archer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : Villager
+{
+    public GameObject arrowPrefab;
+    public Transform spawnPoint;
+    public float delay = 0.466f;
+
+    protected override void Attack()
+    {
+        destination = transform.position;
+        base.Attack();
+        Invoke("SpawnArrow", delay);
+    }
+
+    void SpawnArrow()
+    {
+        Instantiate(arrowPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
