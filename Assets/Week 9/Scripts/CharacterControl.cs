a5695e1e5a58b080b982dcd620f47ce63717744c
simranBaria <simranbaria06@gmail.com>
2024-03-21 18:12:34 -0400

    Task 4a complete: villagers have world-space canvases and and are selected by a dropdown

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -10,6 +10,8 @@ public class CharacterControl : MonoBehaviour
     public static Villager SelectedVillager { get; private set; }
     public TextMeshProUGUI selectedText;
     public static CharacterControl Instance;
+    public TMP_Dropdown dropdown;
+    public Villager[] villagers = new Villager[3];
 
     public void Start()
     {
@@ -31,5 +33,13 @@ public class CharacterControl : MonoBehaviour
     {
         /*if (SelectedVillager != null) selectedText.SetText("Selected: " + SelectedVillager.name);
         else selectedText.SetText("Selected: None");*/
+    }
+
+    public void ChangeVillager(Int32 value)
+    {
+        for(int i = 0; i < villagers.Length; i++)
+        {
+            if (dropdown.options[value].text == villagers[i].name) SetSelectedVillager(villagers[i]);
+        }
     }
 }
